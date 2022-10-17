530f400e7e340f522f2474b024498e0ee0acde34
nicole1795 <nicoledupyt95@gmail.com>
2022-10-17 11:35:46 +0200

    Version2 17-10-2022

--- src/main/java/org/openjfx/PrimaryController.java
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -2,8 +2,11 @@ package org.openjfx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
+
+    public Button primaryButton;
 
     @FXML
     private void switchToSecondary() throws IOException {
